bb49c2d29d572296abd1565bbf500b362d2e6376
Jannik Reimers <me@jansel.dev>
2024-10-29 23:16:57 +0100

    forgot this

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 pluginManagement {
 	plugins {
 		// Update this in libs.version.toml when you change it here.
-		kotlin("jvm") version "2.0.20"
-		kotlin("plugin.serialization") version "2.0.20"
+		kotlin("jvm") version "2.0.21"
+		kotlin("plugin.serialization") version "2.0.21"
 
 		id("com.github.jakemarsden.git-hooks") version "0.0.2"
 		id("com.github.johnrengelman.shadow") version "8.1.1"
