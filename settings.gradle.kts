4d5ba6754a0f5c1c95c5e2fe0698cd01adc31496
Jannik Reimers <me@jansel.dev>
2025-06-16 12:40:46 +0200

    [skip ci] remove plugins from settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,4 @@
 pluginManagement {
-	plugins {
-		// Update this in libs.version.toml when you change it here.
-		kotlin("jvm") version "2.1.21"
-		kotlin("plugin.serialization") version "2.1.21"
-
-		id("com.github.johnrengelman.shadow") version "8.1.1"
-
-		id("dev.kordex.gradle.docker") version "1.7.1"
-		id("dev.kordex.gradle.kordex") version "1.7.1"
-	}
 	repositories {
 		gradlePluginPortal()
 		mavenCentral()
